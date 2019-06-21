94fe7954a7d8a213ac08d63c0a747dbb9423e4ac
eyvind <eyvind@192.168.0.107>
2019-06-21 14:20:32 +0200

    asdas

--- src/formatter/Formatter.java
+++ src/formatter/Formatter.java
@@ -1,34 +1,43 @@
 package formatter;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Formatter {
 
 	public static void main(String[] args) {
 
+		Formatter main = new Formatter();
+		ArrayList<String> list = main.readAndSort();
+		main.writeAndCount(list);
+
+	}
+
+	private ArrayList<String> readAndSort() {
 		String textToRead = null;
 		File file = new File("src/formatter/kunder.txt");
 		ArrayList<String> list = new ArrayList<>();
 		try (BufferedReader instream = new BufferedReader(new FileReader(file))) {
 			while ((textToRead = instream.readLine()) != null) {
-				if(textToRead.length()>0)
-				{
-				StringBuilder stringBuilder= new StringBuilder(textToRead);
-				if(stringBuilder.charAt(0)=='0')
-					stringBuilder.deleteCharAt(0);
-				int position=stringBuilder.indexOf("-");
-				if(position!=-1)
-				stringBuilder.deleteCharAt(position);
-				//!!if(stringBuilder.charAt(0)!='+')
-				//!!stringBuilder.insert(0, "+46");
-				//stringBuilder.trimToSize();//??
-				list.add(stringBuilder.toString());//stringBuilder.toString();??
+				if (textToRead.length() > 0) {
+					StringBuilder stringBuilder = new StringBuilder(textToRead);
+					if (stringBuilder.charAt(0) == '0')
+						stringBuilder.deleteCharAt(0);
+					int position = stringBuilder.indexOf("-");
+					if (position != -1)
+						stringBuilder.deleteCharAt(position);
+					if (stringBuilder.charAt(0) != '+')
+						stringBuilder.insert(0, "+46");
+					list.add(stringBuilder.toString());
 				}
 			}
 		} catch (FileNotFoundException e) {
@@ -36,15 +45,25 @@ public class Formatter {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		Collections.sort(list);
-		//remove these//!!
-		ArrayList<String> list2 = new ArrayList<>();
-		list2.add("dr");
-		list2.add("alban");
-		Collections.sort(list2);
-		Collections.sort(list);
-		System.out.println("ddd");//remove!!
+		return list;
+	}
 
+	private void writeAndCount(ArrayList<String> list) {
+		Set<String> set = new TreeSet<String>(list);
+		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/formatter/kunder2.txt"));) {
+
+			for (String r : set) {
+				int frequency=Collections.frequency(list, r);
+				if(frequency>1)
+				{
+				bufferedWriter.write(r + ";" + frequency);
+				bufferedWriter.newLine();
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
